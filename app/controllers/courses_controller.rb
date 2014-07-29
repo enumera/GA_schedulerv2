6b929930242126ecf65107aecb2d6792184f204e
Andrew Fyfe <enumera2000@yahoo.co.uk>
2014-07-29 17:37:40 +0100

    Working on creating course view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,6 +17,7 @@ class CoursesController < ApplicationController
   # GET /courses/1.json
   def show
     @course = Course.find(params[:id])
+    @lessons = @course.lessons
   
     respond_to do |format|
       format.html # show.html.erb
